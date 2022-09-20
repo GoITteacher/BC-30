f72b5a009d1db232d3edcbecd3d4c37e37f28731
Пащенко Владимир <pikimelposhta@gmail.com>
2022-09-20 09:05:23 +0300

    Clear code

--- js/02-forms.js
+++ js/02-forms.js
@@ -6,18 +6,3 @@
  */
 
 const form = document.querySelector('.js-register-form');
-
-form.addEventListener('submit', onFormSubmit);
-
-function onFormSubmit(event) {
-  event.preventDefault();
-
-  const formData = new FormData(event.currentTarget);
-
-  console.log(formData);
-
-  formData.forEach((value, name) => {
-    console.log('onFormSubmit -> name', name);
-    console.log('onFormSubmit -> value', value);
-  });
-}
--- js/03-input-events.js
+++ js/03-input-events.js
@@ -18,22 +18,3 @@ const refs = {
 // refs.input.addEventListener('blur', onInputBlur);
 // refs.input.addEventListener('change', onInputChange);
 // refs.input.addEventListener('input', onInputChange);
-
-refs.input.addEventListener('input', onInputChange);
-refs.licenseCheckbox.addEventListener('change', onLicenseChange);
-
-function onInputFocus() {
-  console.log('Инпут получил фокус - событие focus');
-}
-
-function onInputBlur() {
-  console.log('Инпут потерял фокус - событие blur');
-}
-
-function onInputChange(event) {
-  refs.nameLabel.textContent = event.currentTarget.value;
-}
-
-function onLicenseChange(event) {
-  refs.btn.disabled = !event.currentTarget.checked;
-}
