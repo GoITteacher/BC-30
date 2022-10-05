3b9d0d0445e3c867d33beed0b212c60b4ed4d446
Пащенко Владимир <pikimelposhta@gmail.com>
2022-10-05 09:03:51 +0300

    init

---
+++ src/js/books.js
@@ -0,0 +1,86 @@
+import '../css/common.css';
+import '../css/book.css';
+import { BooksAPI } from './modules/booksAPI';
+import cardBooks from '../templates/card-books.hbs';
+function callback() {
+  console.log('CALLBACK');
+}
+
+const booksApi = new BooksAPI(0, 5);
+
+const refs = {
+  createForm: document.querySelector('.js-create-form'),
+  updateForm: document.querySelector('.js-update-form'),
+  resetForm: document.querySelector('.js-reset-form'),
+  btnLoadMore: document.querySelector('.js-btn-load'),
+  bookList: document.querySelector('.js-articl-list'),
+  deleteForm: document.querySelector('.js-delete-form'),
+};
+
+refs.btnLoadMore.addEventListener('click', onBtnLoadClick);
+refs.createForm.addEventListener('submit', onCreateForm);
+refs.resetForm.addEventListener('submit', onResetForm);
+refs.updateForm.addEventListener('submit', onUpdateForm);
+refs.deleteForm.addEventListener('submit', onDeleteForm);
+
+async function onCreateForm(event) {
+  event.preventDefault();
+  const createdBook = await booksApi.test();
+}
+
+async function onResetForm(event) {
+  event.preventDefault();
+
+  let formData = new FormData(refs.resetForm);
+
+  let book = {};
+  formData.forEach((value, key) => {
+    book[key.replace('book', '').toLowerCase()] = value;
+  });
+  let id = book.id;
+  delete book.id;
+
+  await booksApi.replaceBook(book, id);
+  const books = await booksApi.getBooks();
+  renderBooks(books);
+
+  refs.resetForm.reset();
+}
+
+async function onUpdateForm(event) {
+  event.preventDefault();
+  let book = {};
+  let formData = new FormData(refs.updateForm);
+  formData.forEach((value, key) => {
+    if (value.trim().length > 0)
+      book[key.replace('book', '').toLowerCase()] = value;
+  });
+  let id = book.id;
+  delete book.id;
+
+  await booksApi.updateBook(book, id);
+  const books = await booksApi.getBooks();
+  renderBooks(books);
+
+  refs.updateForm.reset();
+}
+
+async function onDeleteForm(event) {
+  event.preventDefault();
+  let id = refs.deleteForm.elements.bookId.value;
+  const delateBook = await booksApi.deleteBook(id);
+  const books = await booksApi.getBooks();
+  renderBooks(books);
+  console.log(`Book ${delateBook.title} deleted!`);
+}
+
+async function onBtnLoadClick(event) {
+  const books = await booksApi.getBooks();
+  renderBooks(books);
+}
+
+function renderBooks(books) {
+  refs.bookList.innerHTML = cardBooks(books);
+}
+
+booksApi.getLimitBooks();
---
+++ src/js/modules/booksAPI.js
@@ -0,0 +1,110 @@
+import axios from 'axios';
+import { faker } from '@faker-js/faker';
+faker.seed(0);
+faker.locale = 'ru';
+const baseURL = 'http://localhost:4040';
+
+const axiosUp = axios.create({
+  baseURL: `${baseURL}/books`,
+});
+
+export class BooksAPI {
+  constructor(page, limit) {
+    this._page = page;
+    this._limit = limit;
+  }
+
+  async getBooks() {
+    try {
+      const response = await axiosUp.get('');
+      return response.data;
+    } catch {
+      return [];
+    }
+  }
+
+  async test(){
+    const config = {
+      headers:{}
+    }
+    const data = await axios.post('http://localhost:3000/getData', {
+      "asset": "USDT",
+      "countries": [],
+      "fiat": "UAH",
+      "page": 1,
+      "payTypes": [],
+      "proMerchantAds": false,
+      "publisherType": null,
+      "rows": 10,
+      "tradeType": "BUY",
+      "transAmount": "1000"
+    },config)
+
+    console.log(data);
+  }
+
+  async getLimitBooks() {
+    this._page++;
+    try {
+      const response = await axiosUp.get(
+        `?_page=${this._page}&_limit=${this._limit}`,
+      );
+      if (response.status !== 200) {
+        const myError = new Error('no response from server');
+        throw myError;
+      }
+      return response.data;
+    } catch (error) {
+      console.dir(error);
+    }
+  }
+
+  async createBook(book) {
+    try {
+      const response = await axiosUp.post('', book);
+      return response.data;
+    } catch (error) {
+      console.log(error);
+      return [];
+    }
+  }
+
+  async replaceBook(book, id) {
+    try {
+      const response = await axiosUp.put(`/${id}`, book);
+      return response.data;
+    } catch {
+      console.log('Id is undefined');
+      return [];
+    }
+  }
+
+  async updateBook(book, id) {
+    try {
+      const response = await axiosUp.patch(`/${id}`, book);
+      return response.data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async deleteBook(id) {
+    try {
+      const response = await axiosUp.delete(`/${id}`);
+      return response.data;
+    } catch (error) {
+      console.log(error);
+      return {
+        title: 'Default',
+      };
+    }
+  }
+
+  static getRandomBook() {
+    return {
+      author: faker.name.firstName(),
+      desc: faker.lorem.paragraph(),
+      title: faker.word.preposition(),
+    };
+  }
+}
